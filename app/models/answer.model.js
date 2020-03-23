d7e60e168e9fb3b8268564e0f1fdfd70f6d6756d
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2020-03-23 18:05:57 +0100

    selection et suppresion de quiz

--- app/models/answer.model.js
+++ app/models/answer.model.js
@@ -2,6 +2,7 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Question', {
+  id: Joi.number(),
   value: Joi.string().required(),
   isCorrect: Joi.string(),
   type: Joi.boolean(),
--- app/models/quiz.model.js
+++ app/models/quiz.model.js
@@ -2,8 +2,9 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Quiz', {
+  id: Joi.number(),
   theme: Joi.string().required(),
-  name: Joi.string().required(),
+  label: Joi.string().required(),
   dateCreation: Joi.date(),
   dateModification: Joi.date(),
 })
