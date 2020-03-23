559a8771cffe90bb75f6169cd50840b13e3839c3
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2020-03-23 20:17:32 +0100

    fixing

--- app/models/quiz.model.js
+++ app/models/quiz.model.js
@@ -4,7 +4,9 @@ const BaseModel = require('../utils/base-model.js')
 module.exports = new BaseModel('Quiz', {
   id: Joi.number(),
   theme: Joi.string().required(),
+  subTheme: Joi.string(),
   label: Joi.string().required(),
+  difficulty: Joi.string(),
   dateCreation: Joi.date(),
-  dateModification: Joi.date(),
+  dateModification: Joi.date()
 })
