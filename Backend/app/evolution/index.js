9b613680c536d3a06919825d7aea3f161c6c00e9
HONYIGLOH Jean Armand <55703251+RmannLePhilosopheFou@users.noreply.github.com>
2020-04-06 23:10:09 +0200

    Backend et services deroulement du jeu #10

---
+++ Backend/app/evolution/index.js
@@ -0,0 +1,57 @@
+const { Router } = require('express')
+const { Evolution } = require('../utils/base-model')
+const questionPlayedRouter = require('./questionPlayed')
+
+const router = new Router()
+
+router.post('/', (req, res) => {
+  try {
+    const quiz = Evolution.create({ ...req.body })
+    res.status(201).json(quiz)
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(400).json(err.extra)
+    } else {
+      res.status(500).json(err)
+    }
+  }
+})
+
+router.get('/', (req, res) => {
+  try {
+    res.status(200).json(Evolution.get())
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+router.get('/:evolutionId', (req, res) => {
+  try {
+    const quiz = Evolution.getById(req.params.evolutionId)
+    res.status(200).json(quiz)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.delete('/:evolutionId', (req, res) => {
+  try {
+    Evolution.delete(req.params.evolutionId)
+    res.status(200).json('deleted')
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.put('/:evolutionId', (req, res) => {
+  try {
+    const quiz = Evolution.update(req.params.evolutionId, { ...req.body })
+    res.status(200).json(quiz)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+
+router.use('/:evolutionId/questions', questionPlayedRouter)
+
+module.exports = router
