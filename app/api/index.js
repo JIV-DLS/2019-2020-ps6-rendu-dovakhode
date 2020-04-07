49688bd1cc37285a38bdf86c08a0601910facb15
HONYIGLOH Jean Armand <55703251+RmannLePhilosopheFou@users.noreply.github.com>
2020-04-07 15:19:40 +0200

    Evolution and questionPlayed #10

--- app/api/index.js
+++ app/api/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
 const QuizzesRouter = require('./quizzes')
+const EvolutionRouter = require('./evolution')
 
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
 router.use('/quizzes', QuizzesRouter)
+router.use('/evolution', EvolutionRouter)
 module.exports = router
--- app/models/QuestionPlayed.model.js
+++ app/models/QuestionPlayed.model.js
@@ -3,7 +3,8 @@ const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('QuestionPlayed', {
   id: joi.number(),
-  // questionsPlayed: any[],
-  EvolutionId: joi.number().required(),
+  trials: joi.number(),
+  idQuestion: joi.number(),
+  EvolutionId: joi.number(),
 
 })
