dc172613918c388b17a4039aed7719dd32f3cdfe
AymericRebuffel <aymeric.rebuffel@etu.unice.fr>
2020-04-07 11:26:32 +0200

    Fixinx linting error

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, Input, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {Question} from '../../models/question.model';
+import {QuestionService} from '../../services/question.service';
 
 @Component({
   selector: 'app-questions',
@@ -11,14 +12,22 @@ export class QuestionsComponent implements OnInit {
   @Input() questionEdition = null;
   constructor(
     public dialogRef: MatDialogRef<QuestionsComponent>,
+    // public questionService: QuestionService,
     @Inject(MAT_DIALOG_DATA) public questions: Question[]) {}
 
 
   ngOnInit(): void {
     if (this.questionEdition != null) {this.questions = this.questionEdition; }
   }
-  addQuestion(question: Question) {
-    this.questions.push(question);
+  addQuestion(questionToCreate: Question) {
+    /*this.questionService.addQuestion(questionToCreate).subscribe((question) => {
+      if (question !== undefined) {
+        this.questions.push(question);
+        this.close();
+      }
+    });*/
+    this.questions.push(questionToCreate);
+    this.close();
   }
 
   close() {
