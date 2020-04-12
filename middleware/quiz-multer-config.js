ef18ea48d87882a744d568a7f239a94218dd68f2
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2020-04-12 18:12:51 +0200

    post de plusieurs images #15

--- middleware/quiz-multer-config.js
+++ middleware/quiz-multer-config.js
@@ -11,10 +11,12 @@ const storage = multer.diskStorage({
     callback(null, 'images/quiz')
   },
   filename: (req, file, callback) => {
+    console.log(file.originalname)
     const name = file.originalname.split(' ').join('_')
     const extension = MIME_TYPES[file.mimetype]
     callback(null, `${name + Date.now()}.${extension}`)
   },
 })
 
-module.exports = multer({ storage }).single('image')
+// module.exports = multer({ storage }).single('image')
+module.exports = multer({ storage }).array('image', 5)
