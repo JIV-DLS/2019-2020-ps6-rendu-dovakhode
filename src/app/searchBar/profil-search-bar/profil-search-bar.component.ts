ddedb8405f73e66a49f7b6d66fad6b80331a693b
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2020-04-19 02:40:19 +0200

    #22 option: création des Barre de recherche

---
+++ src/app/searchBar/profil-search-bar/profil-search-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-search-bar',
+  templateUrl: './profil-search-bar.component.html',
+  styleUrls: ['./profil-search-bar.component.scss']
+})
+export class ProfilSearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/searchBar/quiz-search-bar/quiz-search-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-search-bar',
+  templateUrl: './quiz-search-bar.component.html',
+  styleUrls: ['./quiz-search-bar.component.scss']
+})
+export class QuizSearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
