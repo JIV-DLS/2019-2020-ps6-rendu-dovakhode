0cd7e90781b7dc020bc77a50639afd3d393d9c70
AymericRebuffel <aymeric.rebuffel@etu.unice.fr>
2020-04-19 10:20:56 +0200

    Evolution de la modification des profils

---
+++ Frontend/src/app/Profils/profil/profil-edit/profil-edit.component.ts
@@ -0,0 +1,68 @@
+import {Component, EventEmitter, Inject, OnInit, Output} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {ProfilServices} from '../../../../services/profil.services';
+import {Profil} from '../../../../models/profil.model';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
+import {ProfilComponent} from '../profil.component';
+import {Question} from '../../../../models/question.model';
+
+@Component({
+  selector: 'app-profil-edit',
+  templateUrl: './profil-edit.component.html',
+  styleUrls: ['./profil-edit.component.scss']
+})
+export class ProfilEditComponent implements OnInit {
+  public profilForm: FormGroup;
+  imagePreview: string;
+  @Output()
+  profileCreated: EventEmitter<Profil> = new EventEmitter<Profil>();
+  constructor(@Inject(MAT_DIALOG_DATA) public profil: Profil,
+              public dialog: MatDialog,
+              private dialogRef: MatDialogRef<ProfilComponent>,
+              private formbuilder: FormBuilder,
+              private profilService: ProfilServices,
+              private Activerouter: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.initiForm();
+  }
+
+  get nom() {
+    return this.profilForm.get('nom') as FormArray;
+  }
+  initiForm() {
+    this.profilForm = this.formbuilder.group({
+      nom: this.profil.nom,
+      age: this.profil.age,
+      prenom: this.profil.prenom,
+      stade: this.profil.stade,
+      sexe: this.profil.sexe,
+      recommandations: this.profil.recommandations,
+      image: this.profil.image
+    });
+  }
+
+  editTheProfil() {
+    if (this.conform()) {
+      const profil: Profil =  (this.profilForm.getRawValue()) as Profil;
+      // profil.tmpUrl = this.profilForm.get('imagePreview').value;
+      this.profileCreated.emit(profil);
+      /* tslint:disable */
+      this.dialogRef.close({
+        profil : this.profil});
+    }
+    /*const form = this.profilForm.value;
+    const profil = ;
+    profil.prenom = form.prenom;
+    profil.nom = form.nom;
+    profil.age = +form.age;
+    profil.stade = form.stade;
+    profil.recommandations = form.recommandations;
+    profil.sexe = this.profilForm.get('sexe').value;*/
+  }
+  conform(){
+    return true;
+  }
+}
--- Frontend/src/app/Profils/profil/profil.component.ts
+++ Frontend/src/app/Profils/profil/profil.component.ts
@@ -1,6 +1,10 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {Component, EventEmitter, Inject, OnInit, Output} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {Profil} from '../../../models/profil.model';
+import {ProfilEditComponent} from './profil-edit/profil-edit.component';
+import {EditQuestionComponent} from '../../questions/edit-question/edit-question.component';
+import {Question} from '../../../models/question.model';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-profil',
@@ -8,10 +12,51 @@ import {Profil} from '../../../models/profil.model';
   styleUrls: ['./profil.component.scss']
 })
 export class ProfilComponent implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public profil: Profil) { }
+  private profilDialogOpened = false;
+  constructor(@Inject(MAT_DIALOG_DATA) public profil: Profil,
+              public dialog: MatDialog,
+              private dialogRef: MatDialogRef<ProfilComponent>,
+              public formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  editProfile() {
+    const dialogRef = this.dialog.open(ProfilEditComponent, {
+      width: '950px',
+      maxHeight: '500px',
+      data: this.profil
+    });
+    dialogRef.afterClosed().subscribe(response => {
+      this.profilDialogOpened = false;
+      if (response != null) {
+        if (response.question && response.question.label) {
+          if (response.question && response.question.label) {
+            this.replaceBy(this.profil, {...this.createProfilByData(response.profil).getRawValue()});
+          }
+        }
+      }
+
+    });
+  }
+  private createProfilByData(profil) {
+      return this.formBuilder.group({
+        nom : profil.nom,
+        age : profil.age,
+        prenom: profil.prenom,
+        stade: profil.stade,
+        sexe: profil.sexe,
+        recommandations: profil.recommandations,
+        image: profil.image
+      });
+    }
+    replaceBy(profil, data) {
+      profil.age = data.age;
+      profil.nom = data.nom;
+      profil.prenom = data.prenom;
+      profil.stade = data.stade;
+      profil.recommandations = data.recommandations;
+      profil.sexe = data.sexe;
+      profil.image = data.image;
+    }
 }
