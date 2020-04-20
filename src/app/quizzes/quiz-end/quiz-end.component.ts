e4d38480353f9ba792bf3d38bf63174c40825c16
SylvainMarsili <sylvain.marsili@etu.univ-cotedazur.fr>
2020-04-20 19:47:05 +0200

    #28 ajout des résultats fin de quiz

--- src/app/quizzes/quiz-end/quiz-end.component.ts
+++ src/app/quizzes/quiz-end/quiz-end.component.ts
@@ -5,6 +5,9 @@ import {environment} from '../../../environments/environment';
 import {QuizService} from '../../../services/quiz.service';
 import {EvolutionService} from '../../../services/evolution.service';
 import {Quiz} from '../../../models/quiz.model';
+import {Evolution} from '../../../models/evolution.model';
+import {QuestionPlayedService} from '../../../services/questionPlayed.service';
+import {QuestionPlayed} from '../../../models/questionPlayed.model';
 
 @Component({
   selector: 'app-quiz-end',
@@ -15,13 +18,25 @@ export class QuizEndComponent implements OnInit {
   id: string;
   idPatient: number;
   quiz: Quiz;
+  evolution: Evolution;
+  questionsPlayed: QuestionPlayed[];
 
   constructor(private route: ActivatedRoute, private router: Router,
-              private cookiesService: CookieService, private quizSevice: QuizService , private evolution: EvolutionService) { }
+              private cookiesService: CookieService, private quizSevice: QuizService ,
+              private evolutionService: EvolutionService, private questionPlayedService: QuestionPlayedService) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params.id;
     this.idPatient = +(this.route.snapshot.params.idPatient);
+    this.evolutionService.getEvolutionById(+this.route.snapshot.paramMap.get('idEvolution')).subscribe((evol) => {
+      this.evolution = new Evolution(evol);
+      this.questionPlayedService.getQuestionPlayed('' + this.evolution.id).subscribe((questions) => {
+        this.questionsPlayed = questions;
+      });
+    });
+    this.quizSevice.getQuizById(+this.id).subscribe((quiz) => {
+      this.quiz = quiz;
+    });
     if (this.cookiesService.check(environment.runningQuiz)) {
       this.cookiesService.delete(environment.runningQuiz, '/');
     }
@@ -33,5 +48,17 @@ export class QuizEndComponent implements OnInit {
 
   begining() {
     this.router.navigate(['quiz-list']);
+  }
+  resultat() {
+    console.log(this.questionsPlayed);
+    const resultat = [ 0 , 0 ];
+    for (const question of this.questionsPlayed) {
+      if (question.trials <= 1) {
+        resultat[0] += 1;
+      } else if (+question.trials === 2) {
+        resultat[1] += 1;
+      }
+    }
+    return resultat;
   }
 }
