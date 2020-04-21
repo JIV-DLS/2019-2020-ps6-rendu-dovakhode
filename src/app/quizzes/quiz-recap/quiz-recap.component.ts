8a654b77fead664ebf49db224c2cf4f3547a2c60
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2020-04-21 12:43:41 +0200

    #29 routes et component pour la récapitulation de quiz

---
+++ src/app/quizzes/quiz-recap/quiz-recap.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {QuizService} from '../../../services/quiz.service';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {Quiz} from '../../../models/quiz.model';
+
+@Component({
+  selector: 'app-quiz-recap',
+  templateUrl: './quiz-recap.component.html',
+  styleUrls: ['./quiz-recap.component.scss']
+})
+export class QuizRecapComponent implements OnInit {
+  public quiz: Quiz;
+  public index = 0;
+  loading: boolean;
+  constructor(private location: Location,
+              private quizService: QuizService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.quizService.getQuizById(+this.route.snapshot.paramMap.get('id'))
+      .subscribe((quiz) => {
+        this.loading = false;
+        this.quiz = quiz;
+      }, (error) => {this.retour(); });
+  }
+
+  precedent() {
+    this.index = this.index > 0 ? this.index - 1 : 0;
+  }
+
+  suivant() {
+    this.index = this.index < this.quiz.questions.length - 1 ? this.index + 1 : this.quiz.questions.length - 1;
+  }
+
+  private retour() {
+  this.location.back();
+  }
+
+}
