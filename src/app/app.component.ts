1f68680c461108bba72b8f32b049576c320eee3b
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2020-04-21 17:59:35 +0200

    #27 correction de l'aside nav
    correction de la fermeture automatique de l'aside sur des pages spécifique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,9 @@ export class AppComponent implements OnInit {
               private location: Location) {
   }
   title = environment.appName;
-  showFiller = false;
   playingState() {
     const paths = this.location.path(true).split('/');
-    return !(paths[1] === 'quiz-do' && paths[3] === undefined) && !(paths[1] === 'quiz-do' && paths[3] === 'end');
+    return !(paths[1] === 'quiz-do' && paths[3] === undefined) && !(paths[1] === 'quiz-do' && (paths[3] === 'end' || paths[3] === 'recap-start' || paths[3] === 'recap'));
   }
   ngOnInit(): void {
     if (this.playingState()) {
