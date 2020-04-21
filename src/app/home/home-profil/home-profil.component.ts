bf261346689be4da6e4ba99f47e29e7a82565a12
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2020-04-21 12:52:04 +0200

    Gestion de profil
    Correction de l'envoi de signal de la modification d'un profil

--- src/app/home/home-profil/home-profil.component.ts
+++ src/app/home/home-profil/home-profil.component.ts
@@ -3,6 +3,7 @@ import {Profil} from '../../../models/profil.model';
 import {DEFAULT_PROFIL} from '../../../mocks/profil-list.mock';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {ProfilComponent} from '../../Profils/profil/profil.component';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-home-profil',
@@ -11,7 +12,7 @@ import {ProfilComponent} from '../../Profils/profil/profil.component';
 })
 export class HomeProfilComponent implements OnInit {
   public searchedProfil: Profil = DEFAULT_PROFIL;
-
+  profilChanged: Subject<null>;
   constructor( public dialog: MatDialog,
                private dialogRef: MatDialogRef<ProfilComponent>) { }
 
@@ -25,6 +26,9 @@ export class HomeProfilComponent implements OnInit {
       height: '80%',
       data: profil,
     });
-
+    dialogRef.afterClosed().subscribe(data => {
+      if (data) {
+        this.profilChanged.next(null); }
+    });
   }
 }
