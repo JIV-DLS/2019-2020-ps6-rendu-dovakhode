4be021f1e104ec003766d7e3bd826fbcd8152fcf
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2020-04-21 10:26:19 +0200

    #25 quiz-add actualisation en cas de chagement

--- Backend/app/models/quiz.model.js
+++ Backend/app/models/quiz.model.js
@@ -4,7 +4,7 @@ const BaseModel = require('../utils/base-model.js')
 module.exports = new BaseModel('Quiz', {
   id: Joi.number(),
   theme: Joi.string().required(),
-  subTheme: Joi.string(),
+  subTheme: Joi.string().allow(null),
   label: Joi.string().required(),
   difficulty: Joi.string(),
   dateCreation: Joi.date(),
