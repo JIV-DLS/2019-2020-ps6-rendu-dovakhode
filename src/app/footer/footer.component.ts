49c7531e35af53b4d326a927f8ae410a42200c25
SylvainMarsili <sylvain.marsili@etu.univ-cotedazur.fr>
2020-04-26 15:44:01 +0200

    #24 quitter un quiz en cliquant sur le barre de progression si en tablette ou mobile

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {EvolutionService} from '../../services/evolution.service';
 import {Location} from '@angular/common';
+import {DeviceDetectorService} from 'ngx-device-detector';
 
 @Component({
   selector: 'app-footer',
@@ -10,19 +11,26 @@ import {Location} from '@angular/common';
 export class FooterComponent implements OnInit {
   @Input()
   isPlaying: boolean;
+  isTablet: boolean;
+  isMobile: boolean;
   progression: {did: number, total: number};
-  constructor(public evolutionService: EvolutionService, private location: Location) { }
+  constructor(public evolutionService: EvolutionService, private location: Location, private deviceService: DeviceDetectorService) {
+      this.isTablet =  this.deviceService.isTablet();
+      this.isMobile = this.deviceService.isMobile();
+  }
 
   ngOnInit(): void {
     this.evolutionService.evolutionProgressValue.subscribe(progression => this.progression = progression);
   }
 
   interruptProgression() {
-    // tslint:disable-next-line:max-line-length
-    if (confirm((this.progression.did > 0 ? (this.progression.did + ' ' + (this.progression.did > 1 ? 'questions jouées' : 'questios joué') + ' sur ' + this.progression.total) : 'Aucune question n\'a été jouée! ' + this.progression.total + ' questions restantes.') +
-      '\nVoulez-vous vraiment retourner au choix de quiz?')) {
-      this.location.back();
-      this.location.back();
+    if ( this.isTablet || this.isMobile ) {
+      // tslint:disable-next-line:max-line-length
+      if (confirm((this.progression.did > 0 ? (this.progression.did + ' ' + (this.progression.did > 1 ? 'questions jouées' : 'questios joué') + ' sur ' + this.progression.total) : 'Aucune question n\'a été jouée! ' + this.progression.total + ' questions restantes.') +
+        '\nVoulez-vous vraiment retourner au choix de quiz?')) {
+        this.location.back();
+        this.location.back();
+      }
     }
   }
 }
